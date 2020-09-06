e02154b809357270a652df2c6ca6793cb8853a55
hex <robert.paul6@gmail.com>
2020-09-06 11:28:37 -0500

    initial commit

--- cli/bw-targs.py
+++ cli/bw-targs.py
@@ -27,12 +27,15 @@ def get_targs(csvfile):
     with open(csvfile, 'r') as csvf:
         csv_reader = csv.DictReader(csvf, quotechar='"')
         for row in csv_reader:
-            hosts = row['Location on Site'].split(";")
-            for host in hosts:
-                host = host.replace('/*', '')
-                host = host.replace('/ mobile', '')
-                host = host + "\n"
-                results.add(host)
+            try:
+                hosts = row['Location on Site'].split(";")
+                for host in hosts:
+                    res = host.replace('/*', '')
+                    res = res.replace('/ mobile', '')
+                    res = res + "\n"
+                    results.add(res)
+            except KeyError as err:
+                print(f"{err} {csvfile}, {dict(row)}")
 
     return {"csvfile": csvfile, "results": results}
 
@@ -42,19 +45,33 @@ def main():
     indir = sys.argv[1]
     outfile = sys.argv[2]
 
+    if "~" in indir:
+        indir = os.path.expanduser(indir)
+    if "~" in outfile:
+        outfile = os.path.expanduser(outfile)
+
+    files = []
     p = Pool()
-    os.chdir(indir)
-    files = glob.glob("*.csv")
+
+    for file in os.listdir(indir):
+        if file.endswith(".csv"):
+            files.append(os.path.join(indir, file))
+
     p.imap_unordered(clean_csv, files)
 
     total = 0
     with open(outfile, 'w') as outf:
         for _ in p.imap_unordered(get_targs, files):
             print(f"[+] Got {len(_.get('results'))} hosts for {_.get('csvfile')}")
+            total += len(_.get('results'))
             for host in _.get('results'):
                 outf.write(host)
-                total += len(_.get('results'))
+
         print(f"[+] Done! Wrote a total of {total} hosts to {outfile}!")
+
+    p.close()
+    p.join()
+    outf.close()
 
 
 if __name__ == "__main__":
